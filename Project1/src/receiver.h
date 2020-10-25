918389ddf6982e4560e72a17078a3de0fca9e0e7
Ana Teresa <anatdiass@gmail.com>
2020-10-25 22:58:49 +0000

    LLOPEN function

---
+++ Project1/src/receiver.h
@@ -0,0 +1,24 @@
+/*Non-Canonical Input Processing*/
+
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+#include <termios.h>
+#include <stdio.h>
+
+#define BAUDRATE B38400
+#define _POSIX_SOURCE 1 /* POSIX compliant source */
+#define FALSE 0
+#define TRUE 1
+
+#define FLAG 0x7e
+#define A 0x03
+#define C 0x03
+
+
+/**
+ * @brief Lê a trama de controlo - SET - e envia a trama UA
+ * 
+ * @param fd 
+ */
+void LLOPEN(int fd);
